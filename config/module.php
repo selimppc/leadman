8db292e3f3ef851fb83c4d364fb47ea5557acf75
Shawon <bd.shawon1991@gmail.com>
2016-06-12 10:15:27 +0600

    Make change for creating modules

---
+++ config/module.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: etsb
+ * Date: 6/12/16
+ * Time: 10:02 AM
+ */
+
+return [
+    'moudles'=> [
+        'admin',
+        'user',
+        'www'
+    ]];
---
+++ modules/admin/routes.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: etsb
+ * Date: 6/12/16
+ * Time: 10:10 AM
+ */
