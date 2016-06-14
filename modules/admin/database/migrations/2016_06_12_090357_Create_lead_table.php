ec4219acc4b06e6c6d1151f8f8b133b4366fbe89
Selim Reza <me@selimreza.com>
2016-06-14 13:15:46 +0600

    updated lead table for status column

--- modules/admin/database/migrations/2016_06_12_090357_Create_lead_table.php
+++ modules/admin/database/migrations/2016_06_12_090357_Create_lead_table.php
@@ -17,7 +17,7 @@ class CreateLeadTable extends Migration
             $table->string('email',256)->nullable();
             $table->unsignedInteger('popping_email_id',false)->nullable();
             $table->foreign('popping_email_id')->references('id')->on('popping_email');
-            $table->string('status',45)->nullable();
+            $table->enum('status',['open', 'close', 'invoiced'])->nullable();
             $table->unsignedInteger('created_by',false)->nullable();
             $table->unsignedInteger('updated_by',false)->nullable();
             $table->timestamps();
