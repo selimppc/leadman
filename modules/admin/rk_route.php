ae7ecf20e1f6c36e538672a46dd328d3ee7ca8ae
Ram Paul <ram@edutechsolutionsbd.com>
2016-06-15 15:19:33 +0600

    middleware added to route

--- modules/admin/rk_route.php
+++ modules/admin/rk_route.php
@@ -3,14 +3,17 @@
 //exit('ok');
 
 Route::any('central-settings', [
+    'middleware'=>'acl_access::central-settings',
     'as' => 'central-settings',
     'uses' => 'CentralSettingsController@index'
 ]);
 Route::any('central-settings-show/{id}', [
+    'middleware'=>'acl_access::central-settings-show/{id}',
     'as' => 'central-settings-show',
     'uses' => 'CentralSettingsController@show'
 ]);
 Route::any('central-settings-edit/{id}', [
+    'middleware'=>'acl_access::central-settings-edit/{id}',
     'as' => 'central-settings-edit',
     'uses' => 'CentralSettingsController@edit'
 ]);
