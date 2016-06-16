f8e16efee7bd5766039ea135c5edfb7c77c2f243
Shawon <bd.shawon1991@gmail.com>
2016-06-16 11:10:07 +0600

    rk_route.php changed

--- modules/admin/rk_route.php
+++ modules/admin/rk_route.php
@@ -2,18 +2,18 @@
 
 //exit('ok');
 
-Route::any('central-settings', [
-    'middleware'=>'acl_access::central-settings',
+Route::any('admin/central-settings', [
+    'middleware'=>'acl_access::admin/central-settings',
     'as' => 'central-settings',
     'uses' => 'CentralSettingsController@index'
 ]);
-Route::any('central-settings-show/{id}', [
-    'middleware'=>'acl_access::central-settings-show/{id}',
+Route::any('admin/central-settings-show/{id}', [
+    'middleware'=>'acl_access::admin/central-settings-show/{id}',
     'as' => 'central-settings-show',
     'uses' => 'CentralSettingsController@show'
 ]);
-Route::any('central-settings-edit/{id}', [
-    'middleware'=>'acl_access::central-settings-edit/{id}',
+Route::any('admin/central-settings-edit/{id}', [
+    'middleware'=>'acl_access::admin/central-settings-edit/{id}',
     'as' => 'central-settings-edit',
     'uses' => 'CentralSettingsController@edit'
 ]);
