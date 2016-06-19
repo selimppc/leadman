95c99974e948b08d8f33d6199f67755e960a3567
shajjadhossain <shajjadhossain81@gmail.com>
2016-06-19 15:21:48 +0600

    add user modified

--- modules/user/Views/user/index.blade.php
+++ modules/user/Views/user/index.blade.php
@@ -52,7 +52,7 @@
                             {{--<th> id </th>--}}
                             <th> Username </th>
                             <th> Email </th>
-                            <th> Department </th>
+                            {{--<th> Department </th>--}}
                             <th> Status &nbsp;&nbsp;<span style="color: #A54A7B" class="user-guideline" data-placement="top" data-content="you can change status from update page"></span></th>
                             <th> Expire Date </th>
                             <th> Action &nbsp;&nbsp;<span style="color: #A54A7B" class="user-guideline" data-placement="top" data-content="view : click for details informations<br>update : click for update informations<br>delete : click for delete informations"></span></th>
@@ -64,7 +64,7 @@
                                 <tr class="gradeX">
                                     <td>{{ucfirst($values->username)}}</td>
                                     <td>{{$values->email}}</td>
-                                    <td>{{isset($values->relDepartment->title)?ucfirst($values->relDepartment->title):''}}</td>
+                                    {{--<td>{{isset($values->relDepartment->title)?ucfirst($values->relDepartment->title):''}}</td>--}}
                                     <td>{{ucfirst($values->status)}}</td>
                                     {{--<td>{{$values->expire_date}}</td>--}}
                                     <td>{{date('Y-m-d', strtotime($values->expire_date))}}</td>
