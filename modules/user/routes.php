3850a6ab81b8b89b1fe1233d6b82cd4609780c16
Ram Paul <ram@edutechsolutionsbd.com>
2016-06-20 15:12:00 +0600

    user route has been changed

--- modules/user/routes.php
+++ modules/user/routes.php
@@ -57,50 +57,50 @@ Route::group(array('modules'=>'User', 'namespace' => 'Modules\User\Controllers')
     /**Menu Panel**/
 
     Route::get("menu-panel", [
-        //"middleware" => "acl_access:branch",
+        "middleware" => "acl_access:menu-panel",
         "as"   => "menu-panel",
         "uses" => "MenuPanelController@index"
     ]);
 
     Route::any("store-menu-panel", [
-        //"middleware" => "acl_access:store-branch",
+        "middleware" => "acl_access:store-menu-panel",
         "as"   => "store-menu-panel",
         "uses" => "MenuPanelController@store"
     ]);
 
     Route::any("search-menu-panel", [
-        //"middleware" => "acl_access:search-menu-panel",
+        "middleware" => "acl_access:search-menu-panel",
         "as"   => "search-menu-panel",
         "uses" => "MenuPanelController@search_menu_panel"
     ]);
 
     Route::any("view-menu-panel/{id}", [
-        //"middleware" => "acl_access:view-branch/{id}",
+        "middleware" => "acl_access:view-menu-panel/{id}",
         "as"   => "view-menu-panel",
         "uses" => "MenuPanelController@show"
     ]);
 
 
     Route::any("edit-menu-panel/{id}/{parent_menu_id}", [
-        //"middleware" => "acl_access:edit-branch/{id}",
+        "middleware" => "acl_access:edit-menu-panel/{id}/{parent_menu_id}",
         "as"   => "edit-menu-panel",
         "uses" => "MenuPanelController@edit"
     ]);
 
     Route::any("update-menu-panel/{id}", [
-        //"middleware" => "acl_access:update-branch/{id}",
+        "middleware" => "acl_access:update-menu-panel/{id}",
         "as"   => "update-menu-panel",
         "uses" => "MenuPanelController@update"
     ]);
 
     Route::any("delete-menu-panel/{id}", [
-        //"middleware" => "acl_access:delete-branch/{id}",
+        "middleware" => "acl_access:delete-menu-panel/{id}",
         "as"   => "delete-menu-panel",
         "uses" => "MenuPanelController@delete"
     ]);
 
     Route::any('menu-list', [
-        //'middleware' => 'acl_access:exchange-rate',
+        'middleware' => 'acl_access:menu-list',
         'as' => 'menu-list',
         'uses' => 'MenuPanelController@get_ajax_menu_list'
     ]);
