2dae8b18c052e7b133f010f34f87f51d4d1bc68a
shajjadhossain <shajjadhossain81@gmail.com>
2016-06-22 15:04:11 +0600

    imap,smtp,schedule seed data add

---
+++ modules/admin/database/sql_dump/imap.sql
@@ -0,0 +1,3 @@
+INSERT INTO `imap` (`id`, `name`, `host`, `port`, `created_by`, `updated_by`, `created_at`, `updated_at`) VALUES
+(1, 'Gmail-Imap', 'imap.gmail.com', 993, 1, 1, '2016-06-19 22:11:01', '2016-06-19 22:11:40'),
+(2, 'Yahoo Imap', 'imap.mail.yahoo.com', 993, 1, NULL, '2016-06-19 22:11:24', '2016-06-19 22:11:24');
