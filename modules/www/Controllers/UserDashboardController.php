20ffba264c3a507aadd2acd7fc60251f5d132804
Shawon <bd.shawon1991@gmail.com>
2016-06-27 16:40:53 +0600

    make change on user dashboard

--- modules/www/Controllers/UserDashboardController.php
+++ modules/www/Controllers/UserDashboardController.php
@@ -31,17 +31,20 @@ class UserDashboardController extends Controller
 
         //24 data
         $sql_24_sql = "select
-  p.email,
+ p.email,
  IFNULL(l.nol, 0) AS no_of_lead,
+ IFNULL(l.lc, 0) AS lead_cost,
  IFNULL(i.noi, 0) AS no_of_invoice,
- IFNULL(i.tc, 0) AS total_cost
-from popping_email as p
+ IFNULL(i.tc, 0) AS invoice_cost
+from
+popping_email as p
 
- LEFT JOIN (select id, popping_email_id, count(id) as nol from lead where status != 'close' and lead.status != 'filtered' and lead.created_at > '$last_24' group by popping_email_id ) l on (l.popping_email_id = p.id)
+ LEFT JOIN (select lead.id, lead.popping_email_id, count(lead.id) as nol, sum(popping_email.price) as lc from lead INNER JOIN popping_email on popping_email.id = lead.popping_email_id where lead.status != 'close' and lead.status != 'filtered' and lead.created_at > '$last_24' group by lead.popping_email_id ) l on (l.popping_email_id = p.id)
 
- LEFT JOIN (select id, user_id, sum(total_cost) as tc, count(id) as noi from invoice_head where status != 'cancel' and invoice_head.created_at > '$last_24' group by user_id ) i on (i.user_id = p.user_id)
+LEFT JOIN (select invoice_head.id, invoice_head.user_id, invoice_head.popping_email_id,  sum(invoice_head.total_cost) as tc, count(invoice_head.id) as noi from invoice_head where invoice_head.status != 'cancel' and invoice_head.created_at > '$last_24' group by popping_email_id ) i on (i.popping_email_id = p.id)
 
- WHERE p.user_id = '$user_id' and p.status != 'cancel' ";
+WHERE p.user_id = '5' and p.status != 'cancel'
+";
         $result_24 = DB::select(DB::raw($sql_24_sql));
         /*$sql_24_sql = "select popping_email.email, count( DISTINCT lead.id ) no_of_lead, count( DISTINCT invoice_head.id) no_of_invoice, sum(DISTINCT invoice_head.total_cost) total_cost
     from popping_email
@@ -56,15 +59,17 @@ from popping_email as p
         $result_7_days_sql = "select
  p.email,
  IFNULL(l.nol, 0) AS no_of_lead,
+ IFNULL(l.lc, 0) AS lead_cost,
  IFNULL(i.noi, 0) AS no_of_invoice,
- IFNULL(i.tc, 0) AS total_cost
-from popping_email as p
+ IFNULL(i.tc, 0) AS invoice_cost
+from
+popping_email as p
 
- LEFT JOIN (select id, popping_email_id, count(id) as nol from lead where status != 'close' and lead.status != 'filtered' and lead.created_at > '$last_24' group by popping_email_id ) l on (l.popping_email_id = p.id)
+ LEFT JOIN (select lead.id, lead.popping_email_id, count(lead.id) as nol, sum(popping_email.price) as lc from lead INNER JOIN popping_email on popping_email.id = lead.popping_email_id where lead.status != 'close' and lead.status != 'filtered' and lead.created_at > '$last_7_days' group by lead.popping_email_id ) l on (l.popping_email_id = p.id)
 
- LEFT JOIN (select id, user_id, sum(total_cost) as tc, count(id) as noi from invoice_head where status != 'cancel' and invoice_head.created_at > '$last_24' group by user_id ) i on (i.user_id = p.user_id)
+LEFT JOIN (select invoice_head.id, invoice_head.user_id, invoice_head.popping_email_id,  sum(invoice_head.total_cost) as tc, count(invoice_head.id) as noi from invoice_head where invoice_head.status != 'cancel' and invoice_head.created_at > '$last_7_days' group by popping_email_id ) i on (i.popping_email_id = p.id)
 
- WHERE p.user_id = '$user_id' and p.status != 'cancel' ";
+WHERE p.user_id = '5' and p.status != 'cancel'";
         $result_7_days = DB::select(DB::raw($result_7_days_sql));
 
 
@@ -94,6 +99,25 @@ from popping_email as p
     GROUP BY popping_email.id ";
         $no_filtered_email = DB::select(DB::raw($no_filtered_email_sql));
 
+        //-- Show invoice Status
+        $invoice_status_sql = "select
+ IFNULL(ih1.oi, 0) AS open_invoice,
+ IFNULL(ih2.ai, 0) AS approved_invoice,
+ IFNULL(ih3.pi, 0) AS paid_invoice,
+ IFNULL(i.tc, 0) AS total_cost
+from invoice_head ih
+
+LEFT JOIN (select id,user_id, count(id) as oi from invoice_head where status != 'cancel' and invoice_head.status = 'open' group by user_id ) ih1 on (ih.user_id = ih1.user_id)
+
+LEFT JOIN (select id,user_id, count(id) as ai from invoice_head where status != 'cancel' and invoice_head.status = 'approved' group by user_id ) ih2 on (ih.user_id = ih2.user_id)
+
+LEFT JOIN (select id,user_id, count(id) as pi from invoice_head where status != 'cancel' and invoice_head.status = 'paid' group by user_id ) ih3 on (ih.user_id = ih3.user_id)
+
+LEFT JOIN (select id, user_id, sum(total_cost) as tc, count(id) as noi from invoice_head where status != 'cancel' group by user_id ) i on (ih.user_id = i.user_id)
+
+where ih.user_id = '$user_id'
+GROUP BY ih.user_id";
+        $invoice_status = DB::select(DB::raw($invoice_status_sql));
 
         return view('www::user_dashboard.index', [
             'result_24' => $result_24,
@@ -101,6 +125,7 @@ from popping_email as p
             'no_of_email' => $no_of_email,
             'no_duplicate_email' => $no_duplicate_email,
             'no_filtered_email' => $no_filtered_email,
+            'invoice_status' => $invoice_status
         ]);
 
     }
--- modules/www/Views/user_dashboard/index.blade.php
+++ modules/www/Views/user_dashboard/index.blade.php
@@ -62,8 +62,9 @@
                         <tr>
                             <th>Popping Email</th>
                             <th>No of Lead</th>
+                            <th>Lead Cost</th>
                             <th>No of Invoice</th>
-                            <th>Total Cost</th>
+                            <th>Invoice Cost</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -72,8 +73,9 @@
                                 <tr>
                                     <td>{{ $result_24->email }}</td>
                                     <td>{{ $result_24->no_of_lead }}</td>
+                                    <td>{{ $result_24->lead_cost }}</td>
                                     <td>{{ $result_24->no_of_invoice }}</td>
-                                    <td>{{ $result_24->total_cost }}</td>
+                                    <td>{{ $result_24->invoice_cost }}</td>
                                 </tr>
                             @endforeach
                         @endif
@@ -142,8 +144,9 @@
                     <tr>
                         <th>Popping Email</th>
                         <th>No of Lead</th>
+                        <th>Lead Cost</th>
                         <th>No of Invoice</th>
-                        <th>Total Cost</th>
+                        <th>Invoice Cost</th>
                     </tr>
                     </thead>
                     <tbody>
@@ -152,8 +155,9 @@
                             <tr>
                                 <td>{{ $result_7_days->email }}</td>
                                 <td>{{ $result_7_days->no_of_lead }}</td>
+                                <td>{{ $result_7_days->lead_cost }}</td>
                                 <td>{{ $result_7_days->no_of_invoice }}</td>
-                                <td>{{ $result_7_days->total_cost }}</td>
+                                <td>{{ $result_7_days->invoice_cost }}</td>
                             </tr>
                         @endforeach
                     @endif
@@ -399,7 +403,80 @@
             </div>--}}
         </div>
     </div>
+    <div class="col-md-12">
 
+        <!-- Javascript -->
+        <script>
+            init.push(function () {
+                var invoice_paid = [
+                    { day: '2014-03-10', v: 20 },
+                    { day: '2014-03-11', v: 10 },
+                    { day: '2014-03-12', v: 15 },
+                    { day: '2014-03-13', v: 12 },
+                    { day: '2014-03-14', v: 5  },
+                    { day: '2014-03-15', v: 5  },
+                    { day: '2014-03-16', v: 20 }
+                ];
+                Morris.Line({
+                    element: 'invoice_paid',
+                    data: invoice_paid,
+                    xkey: 'day',
+                    ykeys: ['v'],
+                    labels: ['Value'],
+                    lineColors: ['#fff'],
+                    lineWidth: 2,
+                    pointSize: 4,
+                    gridLineColor: 'rgba(255,255,255,.5)',
+                    resize: true,
+                    gridTextColor: '#fff',
+                    xLabels: "day",
+                    xLabelFormat: function(d) {
+                        return ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov', 'Dec'][d.getMonth()] + ' ' + d.getDate();
+                    },
+                });
+            });
+        </script>
+        <!-- / Javascript -->
+
+
+        <div class="stat-panel">
+            {{--<div class="stat-cell col-sm-5 bg-primary padding-sm valign-middle">
+                <div id="invoice_paid" class="graph" style="height: 230px;"></div>
+            </div>--}}
+            <div class="stat-cell col-sm-12 padding-sm-hr bordered no-border-r valign-top">
+                <!-- Small padding, without top padding, extra small horizontal padding -->
+                <h4 class="padding-sm no-padding-t padding-xs-hr"><i class="fa fa-cloud-upload text-primary"></i> User Invoice Status </h4>
+                <!-- Without margin -->
+
+                <table class="display table table-bordered table-striped" >
+                    <thead>
+                    <tr>
+                        <th>Total Open Invoices</th>
+                        <th>Total Approved Invoices</th>
+                        <th>Total Paid Invoices</th>
+                        <th>Total Cost</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @if(isset($invoice_status))
+                        @foreach($invoice_status as $user_invoice)
+                            <tr>
+                                <td>{{ $user_invoice->open_invoice }}</td>
+                                <td>{{ $user_invoice->approved_invoice }}</td>
+                                <td>{{ $user_invoice->paid_invoice }}</td>
+                                <td>{{ $user_invoice->total_cost }}</td>
+                            </tr>
+                        @endforeach
+                    @endif
+                    </tbody>
+                </table>
+
+            </div>
+
+
+
+        </div>
+    </div>
 </div>
 
 <script type="text/javascript"> window.jQuery || document.write('<script src="{{ URL::asset('assets/landerapp/js/jquery.min.js') }}">'+"<"+"/script>"); </script>
