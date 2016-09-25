20d1c4d323289812e8ddeb5bbfa28f02c2a2d57b
Selim Reza <me@selimreza.com>
2016-09-25 11:29:57 +0600

    updated sql data for central setting in modules/admin/database/sql_dump/central_settings.sql

--- modules/admin/database/sql_dump/central_settings.sql
+++ modules/admin/database/sql_dump/central_settings.sql
@@ -1,3 +1,5 @@
 INSERT INTO `central_settings` (`id`, `title`, `status`, `user_type`, `created_by`, `updated_by`, `created_at`, `updated_at`) VALUES
 (1, 'Invoice_notification_to_admin', 'yes', 'admin', '1', NULL, '0000-00-00 00:00:00.000000', '0000-00-00 00:00:00.000000'),
-(2, 'invoice-number', '1', 'admin', '1', NULL, '0000-00-00 00:00:00.000000', '0000-00-00 00:00:00.000000');
+(2, 'invoice-number', '1', 'admin', '1', NULL, '0000-00-00 00:00:00.000000', '0000-00-00 00:00:00.000000'),
+(3, 'email-subject-check', 'yes', 'admin', '1', NULL, '0000-00-00 00:00:00.000000', '0000-00-00 00:00:00.000000'),
+(4, 'user-specific-duplicate-lead-yes-or-no', 'yes', 'admin', '1', NULL, '0000-00-00 00:00:00.000000', '0000-00-00 00:00:00.000000');
